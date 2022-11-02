6eb4c661e3aa2868f74bb41fee50e787e766fc67
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-02 11:02:54 +0530

    connected

--- employee.py
+++ employee.py
@@ -1,3 +1,6 @@
+import mysql.connector
+mydb=mysql.connector.connect(host='localhost',user='root',password='',database='employeedb')
+mycursor = mydb.cursor()
 while True:
     print("select an option from the menu")
     print('1 add employee')
@@ -10,6 +13,19 @@ while True:
     choice=int(input('enter the option:'))
     if(choice==1):
         print("add employee")
+        empcode=input('enter empcode:')
+        empname=input("enter empname:")
+        designation=input('enter designation:')
+        salary=input("enter salary:")
+        companyname=input("enter companyname:")
+        phoneno=input("enter phoneno:")
+        emailid=input("enter emailid:")
+        password=input("enter password:")
+        sql='INSERT INTO `employees`(`empcode`, `empname`, `designation`, `salary`, `companyname`, `phoneno`, `emailid`, `password`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+        data=(empcode,empname,designation,salary,companyname,phoneno,emailid,password)
+        mycursor.execute(sql,data)
+        mydb.commit()
+        print("view employee")
     if(choice==2):
         print("view employee")
     elif(choice==3):
